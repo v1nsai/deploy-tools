746ba93356bac254033dd79e639e47a4c95fab6f
doctor_ew <doctor_ew@protonmail.com>
2023-02-14 10:37:46 -0500

    troubleshooting

--- auth/get_token.sh
+++ auth/get_token.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-./env.sh
+[ -f .env ] && source '.env'
+[ -f auth/.env ] && source 'auth/.env'
 
-export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"xsA|n%4E"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}' 
-echo $OS_TOKEN
+export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}' 
+
+echo "$OS_TOKEN"
--- auth/refresh_token.sh
+++ auth/refresh_token.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-./env.sh
+# if [ -f .env ] then; export DOTENV=".env" fi
+# if [ -f auth/.env ] then; export DOTENV="auth/.env" fi
+export DOTENV='auth/.env'
 
-export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"xsA|n%4E"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}' | sed -i '' -e "s/export OS_TOKEN=.*/export OS_TOKEN=$OS_TOKEN/g" .env
+source "$DOTENV"
+
+export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"xsA|n%4E"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}'
+
+echo $DOTENV
 echo $OS_TOKEN
+sed -i '' "s/export OS_TOKEN=.*/export OS_TOKEN=$OS_TOKEN/" "$DOTENV"
+echo "updated OS_TOKEN in $DOTENV"
