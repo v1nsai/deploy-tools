9a752295800ae20e36b371bbb1214be7106b3fb0
doctor_ew <doctor_ew@protonmail.com>
2023-02-14 12:08:46 -0500

    auth scripts working on mac

--- auth/refresh_token.sh
+++ auth/refresh_token.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-# if [ -f .env ] then; export DOTENV=".env" fi
-# if [ -f auth/.env ] then; export DOTENV="auth/.env" fi
-export DOTENV='auth/.env'
+[ -f .env ] && export DOTENV=".env"
+[ -f auth/.env ] && export DOTENV="auth/.env"
+# export DOTENV='auth/.env'
 
 source "$DOTENV"
 echo "$DOTENV"
 
 export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"xsA|n%4E"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}'
 
-echo $DOTENV
-echo $OS_TOKEN
-sed -i '' "s/export OS_TOKEN=.*/export OS_TOKEN=$OS_TOKEN/" "$DOTENV"
+sed -i '' -e "s/export OS_TOKEN=.*/export OS_TOKEN=$OS_TOKEN/g" $DOTENV
 echo "updated OS_TOKEN in $DOTENV"
