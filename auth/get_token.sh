3baef2369d67ab37a66fd7f09b1b4d35a409855a
Dr Ew <doctor_ew@protonmail.com>
2023-02-14 11:21:03 -0500

    troubleshooting

--- auth/get_token.sh
+++ auth/get_token.sh
@@ -3,6 +3,6 @@
 [ -f .env ] && source '.env'
 [ -f auth/.env ] && source 'auth/.env'
 
-export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}' 
+export OS_TOKEN=$(curl -X POST -I $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"$OS_PASSWORD"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -v FS=": " '/^x-subject-token: /{print $2}'
 
 echo "$OS_TOKEN"
--- auth/refresh_token.sh
+++ auth/refresh_token.sh
@@ -5,6 +5,7 @@
 export DOTENV='auth/.env'
 
 source "$DOTENV"
+echo "$DOTENV"
 
 export OS_TOKEN=$(curl -D - -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d '{ "auth": { "identity": { "methods": ["password"],"password": {"user": {"domain": {"name": "'"$OS_USER_DOMAIN_NAME"'"},"name": "'"$OS_USERNAME"'", "password": "'"xsA|n%4E"'"} } }, "scope": { "project": { "domain": { "name": "'"$OS_PROJECT_DOMAIN_NAME"'" }, "name":  "'"$OS_PROJECT_NAME"'" } } }}') | awk -F "[ ',]+" '/x-subject-token:/{print $2}'
 
