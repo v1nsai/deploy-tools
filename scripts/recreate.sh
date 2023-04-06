0785adfdae9886ef6816f35d079a2730f932640d
doctor_ew <doctor_ew@protonmail.com>
2023-04-06 09:54:29 -0400

    using --wait option

--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -2,6 +2,6 @@
 
 source auth/alterncloud.env
 
-openstack stack delete -y $2
-sleep 5
-openstack stack create -t $1 $2
+openstack stack delete -y jump-server --wait
+openstack stack create -t templates/jump-server.yaml jump-server --wait
+openstack stack show jump-server | grep -E 'stack_status|security_groups|output'
