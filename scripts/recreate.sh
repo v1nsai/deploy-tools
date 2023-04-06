03961808a9edbcb214ad4523159947bb956e7dab
doctor_ew <doctor_ew@protonmail.com>
2023-04-06 18:52:11 -0400

    troubleshooting cluster creation

--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -2,6 +2,6 @@
 
 source auth/alterncloud.env
 
-openstack stack delete -y jump-server --wait
-openstack stack create -t templates/jump-server.yaml jump-server --wait
-openstack stack show jump-server | grep -E 'stack_status|security_groups|output'
+openstack stack delete -y $2 --wait
+openstack stack create -t $1 $2 --wait
+openstack stack show $2 | grep -E 'stack_status|security_groups|output'
---
+++ scripts/wordpress-cluster-template-recreate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+openstack coe cluster delete $1
+openstack coe cluster template delete $1
+openstack coe cluster template create $1 \
+    -f yaml \
+    --coe kubernetes \
+    --image 'Fedora CoreOS (37.20230205.3.0-stable)' \
+    --fixed-network private_network \
+    --fixed-subnet private_subnet1 \
+    --external-network External \
+    --keypair techig-site \
+    --dns-nameserver 1.1.1.1 \
+    --dns-nameserver 1.0.0.1 \
+    --flavor alt.gp2.large \
+    --master-flavor alt.gp2.large \
+    --floating-ip-disabled \
+    --volume-driver cinder \
+    --docker-storage-driver overlay \
+    --docker-volume-size 20 \
+    --master-lb-enabled \
+    --network-driver calico
+    # --docker-volume-size 300
+
+openstack coe cluster create $1 \
+    --cluster-template $1 \
+    --master-count 1 \
+    --node-count 1 \
+    --floating-ip-disabled
+
+# this works but ignores disabling floating ip
+openstack coe cluster create kubernetes-v1.23.3-rancher1 \
+    --cluster-template kubernetes-v1.23.3-rancher1 \
+    --master-count 1 \
+    --node-count 1 \
+    --floating-ip-disabled
