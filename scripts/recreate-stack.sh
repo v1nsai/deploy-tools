a11cf7bd4d9a152c34ee38083fcda81fa8f6a103
doctor_ew <doctor_ew@protonmail.com>
2023-04-26 18:23:16 -0400

    update and cleanup

--- scripts/recreate-stack.sh
+++ scripts/recreate-stack.sh
@@ -5,4 +5,4 @@ set -e
 
 openstack stack delete -y $2 --wait | true
 openstack stack create -t $1 $2 --wait
-openstack stack show $2 | grep -E 'stack_status|security_groups|output'
+openstack stack show $2 | grep -E 'stack_status|output'
