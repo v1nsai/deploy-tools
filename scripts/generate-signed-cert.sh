7c08844bcba232d663c2c5ba416defb4e00a96e0
doctor_ew <doctor_ew@protonmail.com>
2023-05-02 21:11:57 -0400

    cloud-provider-openstack cinder install automation

--- scripts/generate-signed-cert.sh
+++ scripts/generate-signed-cert.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Define the filename for the certificate and private key
-CERT_FILE="my-self-signed-cert.pem"
-KEY_FILE="my-private-key.pem"
+set -e
 
-# Generate the private key
-openssl genpkey -algorithm RSA -out $KEY_FILE
+# Create a Certificate Authority (CA)
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -key ca.key -out ca.crt -days 3650 -subj "/C=US/ST=CA/L=San Francisco/O=My Company, Inc./CN=My Company CA"
 
-# Generate the certificate signing request (CSR)
-openssl req -new -key $KEY_FILE -out ${CERT_FILE}.csr
+# Create a certificate and private key
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -subj "/C=US/ST=CA/L=San Francisco/O=My Company, Inc./CN=example.com"
 
-# Generate the self-signed certificate
-openssl x509 -req -days 365 -in ${CERT_FILE}.csr -signkey $KEY_FILE -out $CERT_FILE
+# Use the CA to sign the certificate
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365
 
-# Clean up the CSR file
-rm ${CERT_FILE}.csr
+# Clean up the CSR and serial files
+rm server.csr ca.srl
+
+# Bundle the CA certificate and key into a single file
+cat ca.crt ca.key > ca-bundle.pem
+
+# Bundle the server certificate and key into a single file
+cat server.crt server.key > server-bundle.pem
--- scripts/setup-cloud-provider-cinder.sh
+++ scripts/setup-cloud-provider-cinder.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+set -e
+kubectl delete sc cinder-default
 # Create secret
 kubectl create secret -n kube-system generic cloud-config --from-file=auth/cloud.conf
 
-# Deploy provider
+# Deploy controller manager roles with secret
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/cloud-provider-openstack/master/manifests/controller-manager/cloud-controller-manager-roles.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/cloud-provider-openstack/master/manifests/controller-manager/cloud-controller-manager-role-bindings.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/cloud-provider-openstack/master/manifests/controller-manager/openstack-cloud-controller-manager-ds.yaml
 
+# Deploy Cinder
+# helm repo add cpo https://kubernetes.github.io/cloud-provider-openstack
+# helm repo update
+# helm install cinder-csi cpo/openstack-cinder-csi -f helm/cloud-provider-cinder-values.yaml
+
 # Deploy the CSI Cinder controller plugin
-git clone https://github.com/kubernetes/cloud-provider-openstack
-cd cloud-provider-openstack
-git checkout tags/v1.23.4
-rm manifests/cinder-csi-plugin/csi-secret-cinderplugin.yaml
+# git clone https://github.com/kubernetes/cloud-provider-openstack
+# cd cloud-provider-openstack
+# git checkout tags/v1.23.4
+# rm manifests/cinder-csi-plugin/csi-secret-cinderplugin.yaml
 # update cacerts lines here TODO
 kubectl -f manifests/cinder-csi-plugin/ apply
