e3ee3f4e2077d03be21d78f506d766bff5738241
doctor_ew <doctor_ew@protonmail.com>
2023-05-18 11:59:53 -0400

    https on wp w/ self signed certs

--- scripts/redeploy-cluster-again.sh
+++ scripts/redeploy-cluster-again.sh
@@ -4,6 +4,7 @@ set -e
 source auth/alterncloud.env
 
 openstack server delete wireguard-again --wait || true
+openstack stack delete -y wordpress-again || true
 openstack stack create \
     --parameter "cluster_name=wordpress-again" \
     --parameter "cluster_template_name=wordpress-again-template" \
@@ -15,7 +16,7 @@ projects/algo/setup.sh wireguard-again projects/algo/config.cfg
 
 rm -rf projects/wordpress/.kube-again/config
 openstack coe cluster config wordpress-again --dir projects/wordpress/.kube-again/ --use-certificate
-cp -f projects/wordpress/.kube-again/config ~/.kube/config
+# cp -f projects/wordpress/.kube-again/config ~/.kube/config
 
 exit
 
--- scripts/redeploy-cluster.sh
+++ scripts/redeploy-cluster.sh
@@ -4,6 +4,7 @@ set -e
 source auth/alterncloud.env
 
 openstack server delete wireguard --wait || true
+openstack stack delete -y wordpress --wait || true
 openstack stack create \
     --parameter "cluster_name=wordpress" \
     --parameter "cluster_template_name=wordpress-template" \
@@ -15,7 +16,7 @@ projects/algo/setup.sh wireguard projects/algo/config.cfg
 
 rm -rf projects/wordpress/.kube/config || true
 openstack coe cluster config wordpress --dir projects/wordpress/.kube/ --use-certificate
-cp -f projects/wordpress/.kube/config ~/.kube/config
+# cp -f projects/wordpress/.kube/config ~/.kube/config
 
 exit
 
