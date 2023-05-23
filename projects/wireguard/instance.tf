bc5ee87df57721d2c038f4293491e5386bdb10d0
doctor_ew <doctor_ew@protonmail.com>
2023-05-23 15:29:12 +0000

    wireguard working minus config injection

--- projects/wireguard/instance.tf
+++ projects/wireguard/instance.tf
@@ -9,7 +9,7 @@ resource "openstack_compute_instance_v2" "wireguard" {
   network {
     name = "wordpress"
   }
-
+  depends_on = [ openstack_networking_subnet_v2.wordpress_subnet ]
 #   provisioner "file" {
 #     source      = "config.cfg"
 #     destination = "/opt/config.cfg"
@@ -24,4 +24,28 @@ resource "openstack_compute_floatingip_associate_v2" "myip" {
   floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
   instance_id = "${openstack_compute_instance_v2.wireguard.id}"
   fixed_ip    = "${openstack_compute_instance_v2.wireguard.network.0.fixed_ip_v4}"
+}
+
+resource "openstack_networking_network_v2" "wordpress" {
+  name           = "wordpress"
+  external       = "false"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "wordpress_subnet" {
+  name       = "wordpress_subnet"
+  network_id = "${openstack_networking_network_v2.wordpress.id}"
+  cidr       = "192.168.199.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_router_v2" "wordpress_router" {
+  name                = "wordpress_router"
+  admin_state_up      = true
+  external_network_id = "b1d12129-bbfc-4482-a5d2-c20458459ddc"
+}
+
+resource "openstack_networking_router_interface_v2" "wordpress_subnet_interface" {
+  router_id = "${openstack_networking_router_v2.wordpress_router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.wordpress_subnet.id}"
 }
---
+++ scripts/watch-log.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+watch -cn1 openstack console log show $1 --lines 25
