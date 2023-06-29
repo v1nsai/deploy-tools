1e1e1b592d0f0053fdf3303e7862f0baa0465f78
doctor_ew <doctor_ew@protonmail.com>
2023-06-29 16:35:20 -0400

    scp script

--- projects/wordpress/terraform/wordpress.tf
+++ projects/wordpress/terraform/wordpress.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_instance_v2" "wordpress" {
   name            = "wordpress"
-  image_id        = "5557a492-f9f9-4a8a-98ec-5f642b611d23" # Ubuntu 22.04
+  image_name      = "wordpress"
   flavor_name     = "alt.c2.medium"
   key_pair        = "wordpress"
   security_groups = ["default", "ssh-ingress"]
@@ -30,6 +30,6 @@ output "wordpress_ip" {
 
 resource "openstack_compute_floatingip_associate_v2" "floating_ip_associate" {
   floating_ip = "216.87.32.215"
-  instance_id = "${openstack_compute_instance_v2.wordpress.id}"
-  fixed_ip    = "${openstack_compute_instance_v2.wordpress.network.0.fixed_ip_v4}"
+  instance_id = openstack_compute_instance_v2.wordpress.id
+  fixed_ip    = openstack_compute_instance_v2.wordpress.network.0.fixed_ip_v4
 }
---
+++ scripts/scp-servername.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+source_filename=$1
+destination_filename=$5
+direction=$2
+server_name=$3
+network_name=$4
+
+function show_usage {
+  echo $(
+    cat <<EOF
+    Usage: $0 
+      -s|--source-filename <source_filename> 
+      -d|--destination-filename <destination_filename> 
+      -r|--direction <direction> 
+      -n|--server-name <server_name> 
+      -w|--network-name <network_name>
+EOF
+  )
+}
+
+# # Parse command-line arguments
+# while [[ $# -gt 0 ]]; do
+#   case $1 in
+#   -s | --source-filename)
+#     source_filename=$2
+#     shift
+#     shift
+#     ;;
+#   -d | --destination-file)
+#     destination_filename=$2
+#     shift
+#     shift
+#     ;;
+#   -r | --direction)
+#     direction=$2
+#     shift
+#     shift
+#     ;;
+#   -n | --server-name)
+#     server_name=$2
+#     shift
+#     shift
+#     ;;
+#   -w | --network-name)
+#     network_name=$2
+#     shift
+#     shift
+#     ;;
+#   *)
+#     echo "Invalid option: $1"
+#     show_usage
+#     exit 1
+#     ;;
+#   esac
+# done
+
+# # Check if all required args are provided
+# if [[ -z $source_filename || -z $destination_filename || -z $direction || -z $server_name || -z $network_name ]]; then
+#   echo "Missing required argument."
+#   show_usage
+#   exit 1
+# fi
+
+server_ip=$(openstack server show devbox -f json | jq '.addresses.'$network_name'[] | select(startswith("216"))' | sed 's/"//g')
+
+if [ "$direction" = "to" ]; then
+  scp -r -i ~/.ssh/$3 $1 localadmin@"$server_ip":$4
+elif [ "$direction" = "from" ]; then
+  scp -r -i ~/.ssh/$3 localadmin@"$server_ip":$1 $4
+fi
