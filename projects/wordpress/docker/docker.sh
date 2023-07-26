b59880b662843b4cddab2405097b6feedce547e0
Drew Riffle <doctor_ew@protonmail.com>
2023-07-26 14:55:11 +0000

    wordpress through docker tested and working

--- projects/wordpress/docker/docker.sh
+++ projects/wordpress/docker/docker.sh
@@ -11,9 +11,12 @@ if [ ! -d "/opt/wp-deploy/nginx/ssl" ]; then
     cp -f /opt/wp-deploy/nginx/conf-templates/certbot.conf.template /opt/wp-deploy/nginx/templates/default.conf.template
     
     docker-compose up -d
-    # cp -f /opt/wp-deploy/nginx/conf-templates/default.conf.template /opt/wp-deploy/nginx/templates/default.conf.template
-    # docker-compose restart nginx
+    echo "Starting services..."
+    echo "Sleeping 10s to wait for services to start..."
+    sleep 10
+    cp -f /opt/wp-deploy/nginx/conf-templates/default.conf.template /opt/wp-deploy/nginx/templates/default.conf.template
+    docker-compose restart nginx
 else
-    echo "Using existing certs..."
+    echo "Using existing certs and starting services..."
     docker-compose up -d
 fi
