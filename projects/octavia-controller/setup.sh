f5cd73dd113334427a8de33880b308e801fbf55e
doctor_ew <doctor_ew@protonmail.com>
2023-08-07 10:51:17 -0400

    cleanup

--- projects/octavia-controller/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-source auth/alterncloud.env
-
-kubectl -f projects/octavia-controller/manifest.yaml delete || true
-envsubst < projects/octavia-controller/manifest.yaml | kubectl apply -f -
