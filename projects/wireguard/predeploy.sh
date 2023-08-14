ec9e2f7adf38a8d82e08f0d70d5b53373cfc240d
doctor_ew <doctor_ew@protonmail.com>
2023-08-14 19:19:11 +0000

    switched wireguard to docker

--- projects/wireguard/predeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# algo config.cfg
-CONTENT=$(base64 -w 0 projects/wireguard/config.cfg)
-yq e -i '(.write_files[] | select(.path == "/opt/config.cfg").content) = "'$CONTENT'"' projects/wireguard/cloud-config.yaml
---
+++ projects/wireguard/wireguard.tf
@@ -0,0 +1,18 @@
+resource "openstack_compute_instance_v2" "wireguard" {
+  name            = "wireguard"
+  image_id        = "5557a492-f9f9-4a8a-98ec-5f642b611d23"
+  flavor_name     = "alt.c2.medium"
+  key_pair        = "wireguard"
+  security_groups = ["default", "ssh-ingress", "wireguard-security_group"]
+  user_data       = local.cloud_config
+
+  network {
+    name = "nextcloud"
+  }
+}
+
+resource "openstack_compute_floatingip_associate_v2" "floating_ip_associate" {
+  floating_ip = "216.87.32.38" # openstack_networking_floatingip_v2.floating_ip.address
+  instance_id = openstack_compute_instance_v2.wireguard.id
+  fixed_ip    = openstack_compute_instance_v2.wireguard.network.0.fixed_ip_v4
+}
