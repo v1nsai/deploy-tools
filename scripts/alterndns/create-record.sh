1db2dfb451fac3a409cd9379f9974fbaa03d73d9
doctor_ew <doctor_ew@protonmail.com>
2023-10-05 19:05:17 +0000

    alterndns tested and working

---
+++ scripts/alterndns/create-record.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -e
+
+subdomain="$1"
+domain="$2"
+type="A"
+ip=$(curl ifconfig.me)
+
+echo "Getting token..."
+token=$(curl -s 'https://cloud.alterncloud.com/api/login' \
+-d username="info@techig.com" \
+-d password='M_6anDsm_GPnU2k')
+if echo "$token" | grep -q "error"; then
+    echo "Encountered an error getting a token.  Error: $token"
+else
+    token=$(echo $token | jq -r '.token')
+fi
+
+echo "Getting zone info for $domain..."
+zone=$(curl -sX GET "https://cloud.alterncloud.com/api/dns" \
+    -H "Authorization: Bearer $token" | jq -r '.zones[] | select(.name == "'$domain'")')
+
+# echo "Found record $zone"
+
+echo "Setting service and zone ids..."
+service_id=$(echo $zone | jq -r '.service_id')
+zone_id=$(echo $zone | jq -r '.domain_id')
+
+echo "Getting records for domain: $domain, zone_id: $zone_id, service_id:$service_id..."
+records=$(curl -sX GET "https://cloud.alterncloud.com/api/service/$service_id/dns/$zone_id" \
+    -H "Authorization: Bearer $token" | jq -r '.records[]')
+
+echo "Checking for existing record..."
+existing_record=$(echo $records | jq 'select(.name == "'$subdomain'" and .type == "'$type'")')
+
+if [[ -z "$existing_record" ]]; then
+    echo "Creating new record..."
+    POST_DATA="{
+        \"name\": \"$subdomain.$domain\",
+        \"ttl\": 3600,
+        \"type\": \"A\",
+        \"content\": \"$ip\"
+    }"
+
+    response=$(curl -sX POST "https://cloud.alterncloud.com/api/service/$service_id/dns/$zone_id/records" \
+    -H "Authorization: Bearer $token" \
+    -H "Content-Type: application/json" \
+    -d "${POST_DATA}")
+    success=$(echo $response | jq -r '.success')
+else
+    echo "Editing already existing record..."
+    record_id=$(echo $existing_record | jq -r '.id')
+    if [[ -z "$record_id" ]]; then
+        echo "Record id is empty, full error: $record_id"
+        exit 1
+    fi
+
+    POST_DATA="{
+        \"name\": \"$subdomain.$domain\",
+        \"ttl\": 3600,
+        \"type\": \"A\",
+        \"content\": \"$ip\"
+    }"
+
+    response=$(curl -sX PUT "https://cloud.alterncloud.com/api/service/$service_id/dns/$zone_id/records/$record_id" \
+    -H "Authorization: Bearer $token" \
+    -H "Content-Type: application/json" \
+    -d "${POST_DATA}")
+    success=$(echo $response | jq -r '.success')
+fi
+
+if [[ "$success" == "true" ]]; then
+    echo "Record created/updated successfully!"
+else
+    if [[ "$response" == *"Contents are identical"* ]]; then
+        echo "Record already exists, skipping..."
+    else
+        echo "Something went wrong, full response: $response"
+        exit 1
+    fi
+fi
+echo "Done!"
+rm -- "$0"
--- scripts/alterndns/login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-curl 'https://cloud.alterncloud.com/api/login' \
-    -d username="alt_client_14_3"\
-    -d password='xsA|n%4E'
