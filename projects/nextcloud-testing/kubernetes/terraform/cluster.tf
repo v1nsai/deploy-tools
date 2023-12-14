d6b6fac7dc54a60fa6d69ec100e8ccc2d8be2f14
doctor_ew <doctor_ew@protonmail.com>
2023-12-14 22:20:56 +0000

    cleanup nextcloud-testing

--- projects/nextcloud-testing/kubernetes/terraform/cluster.tf
+++
@@ -1,7 +0,0 @@
-resource "openstack_containerinfra_cluster_v1" "nextcloud-k8s" {
-  name                = "nextcloud-k8s"
-  cluster_template_id = openstack_containerinfra_clustertemplate_v1.nextcloud-k8s.id
-  master_count        = "1"
-  node_count          = "2"
-  keypair             = "nextcloud"
-}
