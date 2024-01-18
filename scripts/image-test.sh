bb597799e4602420cd95264f50c60b73e7e0f515
doctor_ew <doctor_ew@protonmail.com>
2024-01-17 19:55:11 -0500

    kubernetes cert-manager

--- scripts/image-test.sh
+++ scripts/image-test.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
 set -e
-source auth/alterncloud.env
 
-if [[ ! -z "$2" ]] && [[ "$2" == '--rebuild' ]]; then
+REBUILD=false
+ENV=auth/sandbox-openrpc.sh
+while [ $# -ne 0 ]
+do
+    arg="$1"
+    case "$arg" in
+        --rebuild)
+            REBUILD=true
+            ;;
+        --sandbox | --dev)
+            ENV=auth/sandbox-openrpc.sh
+            ;;
+        --production | --prod)
+            ENV=auth/production-openrpc.sh
+            ;;
+        *)
+            PROJECT=$arg
+            ;;
+    esac
+    shift
+done
+
+source $ENV
+
+if $REBUILD; then
     echo "Building new image..."
-    scripts/packer-build.sh $1
+    scripts/packer-build.sh $PROJECT
 fi
 
 echo "Deleting and recreating new instance..."
-scripts/destroy-terraform.sh $1
-scripts/apply-terraform.sh $1
+scripts/destroy-terraform.sh $PROJECT
+scripts/apply-terraform.sh $PROJECT
 
 echo "Switching to instance logs..."
-scripts/watch-log.sh $1
+scripts/watch-log.sh $PROJECT
 
 echo "Connecting to instance..."
-scripts/ssh-servername.sh $1
+scripts/ssh-servername.sh $PROJECT
