33a5f2347707d51c7f4b2a0900e04b7b3eeb6b0f
doctor_ew <doctor_ew@protonmail.com>
2024-01-30 17:52:51 -0500

    moving nextcloud-testing into its own nextcloud submodule & repo

--- projects/nextcloud-helm/terraform/cluster.tf
+++ projects/nextcloud-helm/terraform/cluster.tf
@@ -2,7 +2,8 @@ resource "openstack_containerinfra_cluster_v1" "nextcloud-helm" {
   name                = "nextcloud-helm"
   cluster_template_id = openstack_containerinfra_clustertemplate_v1.nextcloud-helm.id
   master_count        = 1
-  node_count          = 2
+  node_count          = 1
   keypair             = "nextcloud"
   create_timeout      = 30
 }
+x
