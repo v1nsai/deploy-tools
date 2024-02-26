be184fef42315b7ac6fed061c2e77ad331af219b
doctor_ew <doctor_ew@protonmail.com>
2024-02-26 10:30:35 -0500

    fixed permissions bug and made multi environment improvments

--- projects/nextcloud/terraform/instance.tf
+++ projects/nextcloud/terraform/instance.tf
@@ -1,19 +1,26 @@
 resource "openstack_compute_instance_v2" "nextcloud" {
-  name            = "nextcloud-dev"
-  image_name      = "nextcloud-dev"
-  # image_id        = "5557a492-f9f9-4a8a-98ec-5f642b611d23" # Ubuntu 22.04
-  flavor_name     = "alt.st2.medium"
+  # name            = "nextcloud-dev"
+  name            = "nextcloud"
+  # image_name      = "nextcloud-dev" # sandbox
+  image_id        = "5557a492-f9f9-4a8a-98ec-5f642b611d23" # Ubuntu 22.04 for prod
+  flavor_name     = "alt.c2.large"
   key_pair        = "nextcloud"
-  # security_groups = [ "default", "SSH ingress", "HTTP ingress", "HTTPS ingress", "nextcloud-talk" ] # prod names
-  security_groups = [ "default", "ssh-ingress", "http-ingress", "https-ingress" ]
+  security_groups = [ "default", "SSH ingress", "HTTP ingress", "HTTPS ingress", "nextcloud-talk" ] # prod
+  # security_groups = [ "default", "ssh-ingress", "http-ingress", "https-ingress" ] # sandbox
   user_data       = local.cloud_config
 
   network {
-    name = "nextcloud"
+    # name = "nextcloud" # sandbox
+    name = "techigsite" # prod
   }
 }
 
-resource "openstack_compute_floatingip_associate_v2" "floating_ip_associate" {
-  floating_ip = "216.87.32.85"
+resource "openstack_compute_floatingip_associate_v2" "floating_ip_associate" { # sandbox
+  # floating_ip = "216.87.32.85" # sandbox
+  floating_ip = "216.87.32.36" # prod
   instance_id = openstack_compute_instance_v2.nextcloud.id
 }
+ 
+#  resource "openstack_compute_floatingip_v2" "floating_ip" { # prod
+#    pool = "External"
+#  }
